331e03ef1e5cbe1703e1b848b2e55aa0483b1e29
Jeffrey Yu <yujeffrey@gmail.com>
2016-06-26 23:56:46 -0700

    Summarized data

--- run_analysis.R
+++ run_analysis.R
@@ -96,11 +96,10 @@ print("All merged data stored in 'all_data'.")
 
 # 2. Extracts only the measurements on the mean and standard deviation for each
 #    measurement.
-
-
-# 3. Use descriptive activity names to name the activities in the data set.
-
-# 4. Appropriately labels the data set with descriptive variable names.
+means_stds <- grepl("std|mean", names(all_data))
+summary_data <- all_data[, means_stds]
 
 # 5. From the data set in step 4, creates a second, independent tidy data set
 #    with the average of each variable for each activity and each subject.
+
+
